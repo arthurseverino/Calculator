f4392aeecb42041132d1173137752a437ee492fc
Arthur Severino <aas1181@nyu.edu>
2023-09-21 14:38:08 -0400

    Finish project

--- main.js
+++ main.js
@@ -1,80 +1,121 @@
-let output = document.querySelector('.display')
-output.textContent = "loloffgl"
+let prevDisplay = document.querySelector('.previousDisplay')
+prevDisplay.textContent = ""
+
+let currentDisplay = document.querySelector('.currentDisplay')
+currentDisplay.textContent = "0"
+
+//these arrow functions should call functions for clearer, more concise code 
 
 let clear = document.querySelector('.clear')
 clear.addEventListener('click', () => {
-    output.textContent = "0"
+    currentDisplay.textContent = "0"
+    prevDisplay.textContent = ""
+    operator = ''
+    firstOperand = ''
+    secondOperand = ''
+    operandClicked = false;
+
 });
+
+let deletebtn = document.querySelector('.delete');
+deletebtn.addEventListener('click', () => { 
+    currentDisplay.textContent.slice(0,-1);
+    })
+
+let decimal = document.querySelector('#decimal')
+decimal.addEventListener('click', () => {
+    if(clearCurrentDisplay){
+        currentDisplay.textContent = ''
+    };
+    if (currentDisplay.textContent === '') currentDisplay.textContent = '0';
+    if(!(currentDisplay.textContent.includes('.'))){
+        currentDisplay.textContent += "."
+    }
+     
+});
+
 
 let nums = document.querySelectorAll('.num');
 nums.forEach((num) => {
     num.addEventListener('click', () => {
-    if(output.textContent == "0") {
-        output.textContent = num.value
+    if(clearCurrentDisplay == true){
+        currentDisplay.textContent = ''
+        clearCurrentDisplay = false;
+    }
+
+    if(currentDisplay.textContent == "0"){ // || operandClicked) {
+        currentDisplay.textContent = num.value 
     }
     else{
-        output.textContent += num.value;
+        currentDisplay.textContent += num.value;
     }
     })
 })
 
-let decimal = document.querySelector('#decimal')
-decimal.addEventListener('click', () => {
-   // if("." in output.textContent == true){}
-        output.textContent += "."
-});
-
-const add = function (a, b) {
-    return a + b;
-  };
-  
-const subtract = function (a, b) {
-    return a - b;
-  };
-const divide = (a,b) => {a / b}
-const multiply = (a,b) => {a * b} 
-
-
-
-let num1 = parseInt(output.textContent)
+let clearCurrentDisplay = false;
 
 let operator = "";
-
+let firstOperand = ''
 let operands = document.querySelectorAll('.operand')
+let operandClicked = false;
+
+// if I use two different operators, there's a bug 
+
 operands.forEach((operand) => {
-    operands.addEventListener('click', () =>{operator = operand.id})
+    operand.addEventListener('click', () => {
+        operator = operand.textContent; 
+        if(operandClicked){ 
+            secondOperand = currentDisplay.textContent // 3
+            console.log('second operand: ' + secondOperand)
+            firstOperand = operate(firstOperand,operator,secondOperand).toString() // running sum :)
+            console.log('first operand: ' + firstOperand)
+            currentDisplay.textContent = firstOperand 
+            prevDisplay.textContent = `${firstOperand} ${operator}`
+            clearCurrentDisplay = true;
+        }
+        else{
+            firstOperand = currentDisplay.textContent // 69
+            prevDisplay.textContent = `${firstOperand} ${operator} `; // 69 x
+            clearCurrentDisplay = true;
+            operandClicked = true;
+        }
+    })
  })
 
-let num2 = parseInt(output.textContent)
-        //everything from operand index to equal sign index
 
-//we're passing in 2 integers, 1 string
-//and returning the string "result" for display
-function operate(num1,operator,num2){
-    let result = ""
-    if(operator == "add"){
-        result = add(num1,num2).toString()
+let secondOperand = '';
+
+equal = document.querySelector('.equal')
+equal.addEventListener('click', () => {
+    secondOperand = currentDisplay.textContent 
+    prevDisplay.textContent = `${firstOperand} ${operator} ${secondOperand} = ` // 69 x 3 =
+    currentDisplay.textContent = operate(firstOperand, operator, secondOperand); // 207
+    operator = '';
+})
+
+// display snarky message 
+// Use math.round
+
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const divide = (a,b) => a / b;
+const multiply = (a,b) => a * b
+
+
+function operate(firstOperand,operator,secondOperand){
+    firstOperand = Number(firstOperand);
+    secondOperand = Number(secondOperand);
+
+    if(operator == "."){
+        return add(firstOperand,secondOperand)
     }
-    if(operator == "subtract"){
-        result = subtract(num1,num2).toString()
+    if(operator == "-"){
+        return subtract(firstOperand,secondOperand)
     }
     if(operator == "÷"){
-        result = divide(num1,num2).toString()
+        return divide(firstOperand,secondOperand)
     }
-    if(operator == "multiply"){
-        result = multiply(num1,num2).toString()
+    if(operator == "x"){
+        return multiply(firstOperand,secondOperand)
     }
-    output.textContent = result;
 }
-
-//WHENEVER EQUAL IS CLICKED, call operate
-equal = document.querySelector('.equal')
-equal.addEventListener('click', operate(num1, operator, num2))
-
-// once click on decimal 
-
-//i should have another variable for the display value besides output i think 
-
-
-
-
